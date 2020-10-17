e735ee924929b51800e4d4a2bb2927b286f3b716
Test <Test@PMOHAN-IN-LE03.eu.trimblecorp.net>
2020-10-18 00:23:25 +0530

    Add new tests

--- cypress/integration/examples/sample_test.spec.js
+++ cypress/integration/examples/sample_test.spec.js
@@ -1,7 +1,22 @@
-describe('First Test', () => {
-    it('Open ebay and search iphones', () => {
+/// <reference types="Cypress" />
+
+describe('Ebay Tests', () => {
+    
+    beforeEach(() => {
         cy.visit('https://www.ebay.com/')
+    })
+    
+    it('Open ebay.com and Search iphones', () => {
         cy.get('#gh-ac').type('iPhone')
         cy.get('#gh-ac').should('have.value', 'iPhone')
+    });
+
+    it('Verify ebay logo in the Home page', () =>{
+        cy.get('#gh-logo').should('be.visible', true)
+    })
+
+    it('Select a cart and verify the message', () => {
+        cy.get('.gh-cart-icon > use').click({force: true})
+        cy.get('.start-shop').should('not.be.disabled', true)
     });
 });
